a5cd0ee53a90d1c5f0c3723f75d587e0be95a9f8
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-05-22 16:43:43 -0500

    testing

---
+++ test/index.js
@@ -0,0 +1,8 @@
+var assert = require('assert')
+var humansort = require('../index.js')
+
+describe('humansort', function(){
+  it('should be a function', function(){
+    assert.equal(typeof humansort, 'function')
+  })
+})
