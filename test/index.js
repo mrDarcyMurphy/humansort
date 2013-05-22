3a4e160a22bd4ddda95ed0fe4ebb677f19042b76
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-05-22 16:53:26 -0500

    more tests

--- test/index.js
+++ test/index.js
@@ -8,9 +8,27 @@ describe('humansort', function(){
   })
 
   it('should sort numbers', function(){
-    var numbers = [4, 7, 3, 67, 123, 6, 12345]
-    var sortedNumbers = numbers.sort(humansort)
-    assert.deepEqual(sortedNumbers, [ 3, 4, 6, 7, 67, 123, 12345 ])
+    var unsorted = [4, 7, 3, 67, 123, 6, 12345]
+    var sorted = unsorted.sort(humansort)
+    assert.deepEqual(sorted, [ 3, 4, 6, 7, 67, 123, 12345 ])
+  })
+
+  it('should sort letters', function(){
+    var unsorted = ['kjrtuoivcnse', 'skfjh', 'efiufbsd', 'wehjkbf', 'kus', 'asldkjfh']
+    var sorted = unsorted.sort(humansort)
+    assert.deepEqual(sorted, ["asldkjfh","efiufbsd","kjrtuoivcnse","kus","skfjh","wehjkbf"])
+  })
+
+  it('should sort letters and numbers', function(){
+    var unsorted = ['kjrtuoivcnse', 4, 7, 3, 6, 12345, 'skfjh', 67, 123, 'efiufbsd', 'wehjkbf', 'kus', 'asldkjfh']
+    var sorted = unsorted.sort(humansort)
+    assert.deepEqual(sorted, [3,4,6,7,67,123,12345,"asldkjfh","efiufbsd","kjrtuoivcnse","kus","skfjh","wehjkbf"])
+  })
+
+  it('should sort mixed strings', function(){
+    var unsorted = ['#a345!sdfjh', 'skfjh', '456675efiufbsd', 'wh$ehjkbf', 'kus', '1kus', 'kus1', 'asldkjfh']
+    var sorted = unsorted.sort(humansort)
+    assert.deepEqual(sorted, ["#a345!sdfjh","1kus","456675efiufbsd","asldkjfh","kus","kus1","skfjh","wh$ehjkbf"])
   })
 
 })
